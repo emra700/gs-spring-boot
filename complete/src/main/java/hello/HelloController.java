2edcd8f31e67dc13bdf6337f22ecb05379f93e0a
Emil Ramstedt <emira@nets.eu>
2017-03-22 17:58:46 +0100

    OK

--- complete/src/main/java/hello/HelloController.java
+++ complete/src/main/java/hello/HelloController.java
@@ -1,14 +1,50 @@
 package hello;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 public class HelloController {
     
-    @RequestMapping("/")
+    @RequestMapping("/asset")
     public String index() {
         return "Greetings from Spring Boot!";
     }
-    
+
+
+
+    /*
+
+
+
+    {
+  "referenceno":"621273af-9017-4d38-b1bf-a07549aaec0a",
+  "asset":{
+    "keys":[
+      {"email":"jane@server.org"}
+    ],
+    "attributes":[
+      {"firstname":"Jane"},
+      {"lastname":"Doe"}
+    ]
+  }
+}
+
+
+     */
+
+    @RequestMapping(value = "/asset", method = RequestMethod.POST)
+    public String createTask(@RequestBody  String request) {
+
+
+
+
+        return request;
+
+    }
+
+
 }
